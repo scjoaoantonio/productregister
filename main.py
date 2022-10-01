7a27169e51d65cfb495e43ae5f22f0f922d11de4
João Antônio <65183458+scjoaoantonio@users.noreply.github.com>
2022-10-01 16:35:12 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from tkinter import *
 from tkinter import Tk, StringVar, ttk
+from webbrowser import BackgroundBrowser
 
 from PIL import Image, ImageTk
+
+from tkcalendar import Calendar, DateEntry
+from datetime import date
 
 co0 = "#2e2d2b"  # preto
 co1 = "#feffff"  # branco
@@ -36,6 +40,7 @@ frameBaixo = Frame(janela, width=1043, height=300,
                    bg=co1, relief=FLAT)
 frameBaixo.grid(row=2, column=0, pady=0, padx=1, sticky=NSEW)
 
+# Trabalhando no framaCima -------------------------------------
 
 # Manipulando imagem
 app_img = Image.open('./img/book.png')
@@ -45,5 +50,51 @@ app_img = ImageTk.PhotoImage(app_img)
 app_logo = Label(frameCima, image=app_img,
                  text=' Inventário Doméstico', width=900, compound=LEFT, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
 app_logo.place(x=0, y=0)
+
+# Trabalhando no framaMeio -------------------------------------
+
+# Criando entradas
+l_nome = Label(frameMeio, text="Nome", height=1, anchor=NW,
+               font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_nome.place(x=10, y=10)
+e_nome = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_nome.place(x=130, y=11)
+
+l_local = Label(frameMeio, text="Sala/Área", height=1, anchor=NW,
+                font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_local.place(x=10, y=40)
+e_local = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_local.place(x=130, y=41)
+
+l_descricao = Label(frameMeio, text="Descricao", height=1, anchor=NW,
+                    font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_descricao.place(x=10, y=70)
+e_descricao = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_descricao.place(x=130, y=71)
+
+l_model = Label(frameMeio, text="Marca/Model", height=1, anchor=NW,
+                font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_model.place(x=10, y=100)
+e_model = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_model.place(x=130, y=101)
+
+l_cal = Label(frameMeio, text="Data da compra", height=1, anchor=NW,
+              font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_cal.place(x=10, y=130)
+e_cal = DateEntry(frameMeio, width=12, Background='darkblue',
+                  borderwidth=2, year=2022)
+e_cal.place(x=130, y=131)
+
+l_valor = Label(frameMeio, text="Valor da compra", height=1, anchor=NW,
+                font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_valor.place(x=10, y=160)
+e_valor = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_valor.place(x=130, y=161)
+
+l_serial = Label(frameMeio, text="Número de série", height=1, anchor=NW,
+                 font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_serial.place(x=10, y=190)
+e_serial = Entry(frameMeio, width=30, justify='left', relief=SOLID)
+e_serial.place(x=130, y=191)
 
 janela.mainloop()
